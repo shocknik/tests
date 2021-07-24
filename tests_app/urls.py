11f97fb0cf5cc767430104e30fcd3f056cb2052c
shocknik <76256043+shocknik@users.noreply.github.com>
2021-07-24 23:00:24 +0300

    Настройка шаблона (попытка 3)

--- tests_app/urls.py
+++ tests_app/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'tests_app'
 urlpatterns = [
     path('', views.f_list, name='test_list'),
-    path('<int:pk>/', views.f_list, name='test_by_category')
+    path('<int:pk>', views.f_list, name='test_by_category')
 ]
