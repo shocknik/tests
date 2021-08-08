5268dadaf060fd2707c71876bafa60f77d7601a6
shocknik <76256043+shocknik@users.noreply.github.com>
2021-08-08 22:50:23 +0300

    Создание модели заказов

--- tests/settings.py
+++ tests/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'tests_app.apps.TestsAppConfig',
     'cables.apps.CablesConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
